c35592d60497ba3db2e74d91a300f55d6d0fb0a6
Ivan <rivan95@outlook.it>
2018-12-03 16:00:42 +0100

    inizializzato angular

--- server/routes/router.js
+++ server/routes/router.js
@@ -1,14 +1,22 @@
-module.exports = function (app, express){
-    
-    var bodyParser = require('body-parser')
-      app.use(bodyParser.json())
-    //  app.get('/', (req, res) => {
-    //      res.send('ciao mondo');
-   //   }) 
-      app.use('/ricambi', require('../resource/ricambi/index.js'));
-      app.use('/utenti', require('../resource/utenti/index.js'));
-      app.use('/demolitori', require('../resource/demolitori/index.js'));
-      app.use('/auto', require('../resource/auto/index'));
-      app.use('/tiporicambio', require('../resource/tipo_ricambi/index'));
-  }
+module.exports = function (app, express) {
+
+  var bodyParser = require('body-parser')
+  app.use(bodyParser.json())
+  var path = require('path')
+
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', '..', 'public', 'index.html'))
+  })
+
+  app.use('/angular', express.static(path.join(__dirname, '..', '..', 'node_modules', 'angular')))
+  app.use('/public', express.static(path.join(__dirname, '..', '..', 'public')))
+
+
+
+  app.use('/ricambi', require('../resource/ricambi/index.js'));
+  app.use('/utenti', require('../resource/utenti/index.js'));
+  app.use('/demolitori', require('../resource/demolitori/index.js'));
+  app.use('/auto', require('../resource/auto/index'));
+  app.use('/tiporicambio', require('../resource/tipo_ricambi/index'));
+}
 //questa configurazione permette di lanciare nodemon solo tramite npm run dev "dev": "nodemon server/index.js", 
